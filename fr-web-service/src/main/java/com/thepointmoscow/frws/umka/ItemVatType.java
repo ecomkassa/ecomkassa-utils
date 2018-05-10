fb1dcffcb86cee012cf3607c40fbc0873712e5b9
Unlocker <maksimov.sg@gmail.com>
2018-05-11 01:03:04 +0300

    FRWS: Extended available VAT types. Version 0.2.4

--- fr-web-service/src/main/java/com/thepointmoscow/frws/umka/ItemVatType.java
+++ fr-web-service/src/main/java/com/thepointmoscow/frws/umka/ItemVatType.java
@@ -3,12 +3,18 @@ package com.thepointmoscow.frws.umka;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * Value added tax (VAT) types.
+ */
 @AllArgsConstructor
 public enum ItemVatType {
     VAT_18PCT(1),
     VAT_10PCT(2),
-    VAT_20PCT(6), // strange tax number, fallback to None
-    VAT_0PCT(5);
+    VAT_118PCT(3),
+    VAT_110PCT(4),
+    VAT_0PCT(5),
+    VAT_NONE(6),
+    VAT_20PCT(6); // strange tax number, fallback to None
 
     @Getter
     private final int code;
